cc8b57e3d52f442c14469d73cfe775e894cda643
monead <63004649+monead@users.noreply.github.com>
2026-06-30 15:35:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "stringtracker-v1";
+const CACHE = "stringtracker-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
